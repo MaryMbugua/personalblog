66b65285091d084b3661bbf8ff2a78611bec2f8e
Mary Mbugua <marymbugua.nm@gmail.com>
2018-02-14 07:27:51 +0300

    set up mail configurations

--- config.py
+++ config.py
@@ -11,7 +11,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nish:Nish@localhost/personalblog'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY='5678hksdfjdbfhbasfasdfjsjsdk87sfja'
-
+export MAIL_USERNAME='marymbugua.nm@gmail.com'
+export MAIL_PASSWORD='Lou31.Rx'
 python3.6 manage.py server
